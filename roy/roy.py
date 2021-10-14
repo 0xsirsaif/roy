9c0adfb138604f5526f5c1def20162e8b85f452e
Mohamed Saif <sirsaif99@gmail.com>
2021-10-14 06:35:42 +0200

    list of imgs to pdf for presentaion usage

--- roy/roy.py
+++ roy/roy.py
@@ -1,9 +1,9 @@
-import typing
-import subprocess
 import typer
 
 from sys_commands.mount_drive import mount_me
 from sys_commands.github import get_github_token
+
+from scripts.imgs_to_file import pngs_to_pdf
 
 app = typer.Typer()
 
@@ -26,6 +26,11 @@ def mount(drive: str):
 @app.command()
 def github_token():
     typer.echo(get_github_token())
+
+
+@app.command()
+def imgs_to_pdf(src: str, destination: str) -> None:
+    pngs_to_pdf(src, destination)
 
 
 if __name__ == "__main__":
---
+++ roy/scripts/imgs_to_file.py
@@ -0,0 +1,12 @@
+from PIL import Image
+import os
+
+
+def pngs_to_pdf(src: str, destination: str) -> None:
+    len_files = len([img for img in os.listdir(src) if "png" in img])
+    images = [image.convert("RGB") for i in range(1, len_files) if (image := Image.open(os.path.join(src, f"{i}.png")))]
+    img_1 = Image.open(os.path.join(src, "0.png")).convert("RGB")
+
+    file_name = src.split("/")[-2] + ".pdf"  # TODO
+    img_1.save(os.path.join(destination, file_name), save_all=True, append_images=images)
+
