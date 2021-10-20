820eed94770a7a22a68478a2b4e67ce5513856c7
Mohamed Saif <sirsaif99@gmail.com>
2021-10-20 12:52:35 +0200

    make it work. Todo: clean

--- roy/readings/books.py
+++ roy/readings/books.py
@@ -1,61 +1,23 @@
-# import appdirs
-# from cachelib import FileSystemCache
-
-# CACHE_EMPTY_VAL = "NULL"
-# CACHE_DIR = appdirs.user_cache_dir("roy")
-# CACHE_ENTRY_MAX = 12
-
-
-# class NewFileSystemCache(FileSystemCache):
-#     def __init__(
-#         self,
-#         cache_dir: str,
-#         threshold: int = 500,
-#         default_timeout: int = 300,
-#         mode: int = 0o600,
-#     ):
-#         super().__init__(cache_dir, threshold, default_timeout, mode)
-#
-#     def append(self, key, value):
-#         if self.has(key):
-#             super().set(key, value)
-#         else:
-#             super().add(key, value)
-#
-#         print("id added ???", self.get(key))
-#
-#         all_keys = self.get("*")
-#         if all_keys:
-#             new_keys = "".join(k for k in all_keys) + key + ","
-#             print("11111", new_keys)
-#             super().set("*", new_keys)
-#             print("111 all ??", self.get("*"))
-#         else:
-#             added_key = key + ","
-#             print("222222", added_key)
-#             super().add("*", added_key)
-#             print("222 all ??", self.get("*"))
-#         print("id added ???", self.get(key))
-#         print("all ???", self.get("*"))
-#
-#     def remove(self, key):
-#         super().delete(key)
-#
-#         keys = self.get("*").split(",")
-#         key_idx = keys.index(key)
-#         keys.pop(key_idx)
-#         self.set("*", keys)
-
-
-# cache = NewFileSystemCache(CACHE_DIR, CACHE_ENTRY_MAX, default_timeout=0)
-
 import json
+import subprocess
+
+
+def open_book(book):
+    with open("/media/data/projects/roy/roy/readings/Books.json", "r") as f:
+        book_location = [
+            list(j.values())[0] for i, j in enumerate(json.load(f)) if i == int(book)
+        ]
+
+    try:
+        subprocess.run(["xdg-open", book_location[0]])
+    except IndexError:
+        print("No location")
 
 
 def list_books():
     with open("/media/data/projects/roy/roy/readings/Books.json", "r") as f:
-        books = [i for i in json.load(f)]
-        print(books)
+        books = [(i, list(j.keys())) for i, j in enumerate(json.load(f))]
+        return books
 
 
 def add_book(name, location):
--- roy/roy.py
+++ roy/roy.py
@@ -6,7 +6,7 @@ from sys_commands.github import get_github_token
 
 from scripts.imgs_to_file import pngs_to_pdf
 
-from readings.books import list_books, add_book, remove_book, clear_books
+from readings.books import list_books, add_book, remove_book, clear_books, open_book
 
 
 app = typer.Typer()
@@ -42,6 +42,7 @@ def books(
     add: Tuple[str, str] = typer.Option((None, None)),
     rm: Optional[str] = typer.Option(None),
     clear: Optional[str] = typer.Option(None),
+    book: Optional[str] = typer.Option(None),
 ):
     book_name, book_location = add
     if book_name:
@@ -50,6 +51,8 @@ def books(
         remove_book(rm)
     if clear:
         clear_books()
+    if book:
+        open_book(book)
 
     typer.echo(list_books())
 
